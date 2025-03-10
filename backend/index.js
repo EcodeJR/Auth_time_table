d841c3f2fbf16ad2b6c5d01362ffb3e7f224a899
EcodeJR <emmanueldcode@gmail.com>
2025-03-10 20:30:34 +0100

    host

--- backend/index.js
+++ backend/index.js
@@ -14,9 +14,9 @@ const app = express();
 
 // Define allowed origins (make sure they exactly match your deployed frontends)
 const allowedOrigins = [
-  '*',
   'http://localhost:5173',
-  'https://auth-time.vercel.app'
+  'https://auth-time.vercel.app',
+  'https://vercel.com/ecodejrs-projects/auth-time/CAm7c7ZDC2heYPfQcpyx2YWfvRxb'
 ];
 
 app.use(cors({
